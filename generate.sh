8802dcdc07ec60a67df7d32e373d3f15855e9cd1
Nyavro <e.nyavro@gmail.com>
2025-11-17 20:24:03 +0700

    Openapi generation command moved into docker-compose

--- generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -r ./api
-docker run --rm \
-  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
-  -i /local/openapi/openapi.json \
-  -g rust-axum \
-  -o /local/api
