74d897d62afa375b741f695e369bdb2ca1d796ef
Nyavro <e.nyavro@gmail.com>
2026-03-12 21:08:14 +0700

    HW 6.4 introduced ws authentication

--- social/src/modules/post/async_handler.rs
+++ social/src/modules/post/async_handler.rs
@@ -1,3 +1,4 @@
+use axum::http::StatusCode;
 use axum::{
     extract::ws::WebSocketUpgrade,
     response::IntoResponse,
@@ -5,21 +6,27 @@ use axum::{
 use axum::extract::Query;
 use std::sync::Arc;
 use crate::app_state::AppState;
+use crate::modules::auth::auth::verify_token;
 use serde::Deserialize;
-use uuid::Uuid;
 
 
 #[derive(Deserialize)]
 pub struct WebSocketQuery {
-    user_id: Uuid,
+    token: String
 }
 
 pub async fn post_feed_ws_handler(
     ws: WebSocketUpgrade,
     Query(params): Query<WebSocketQuery>,
     axum::extract::State(state): axum::extract::State<Arc<AppState>>,
-) -> impl IntoResponse {        
+) -> impl IntoResponse {      
+    let claims = match verify_token(&params.token, &state.secret.as_bytes()) {
+        Ok(uid) => uid,
+        Err(_) => {
+            return (StatusCode::UNAUTHORIZED, "Invalid token").into_response();
+        }
+    };      
     ws.on_upgrade(move |socket| async move {
-        state.ws_manager.handle_connection(params.user_id, socket).await;}
+        state.ws_manager.handle_connection(claims.user_id, socket).await;}
     )
 }
