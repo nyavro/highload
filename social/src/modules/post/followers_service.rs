77e3d2ca0e6991a63d57f04b24c94d63c72f1d46
Nyavro <e.nyavro@gmail.com>
2026-03-12 20:39:30 +0700

    HW 6.3 Introduced event bus for events listeners

--- social/src/modules/post/followers_service.rs
+++ social/src/modules/post/followers_service.rs
@@ -3,7 +3,7 @@ use crate::modules::friend::repository::{FriendRepository, FriendRepositoryError
 use crate::modules::post::model::Post;
 use crate::modules::post::service_provider::{PostService, PostServiceError};
 use thiserror::Error;
-use log::{error, warn};
+use log::{error, warn, info};
 use async_trait::async_trait;
 use std::sync::Arc; 
 
@@ -11,6 +11,25 @@ use std::sync::Arc;
 pub enum FollowersServiceError {
     #[error("Repository error: {0}")]
     Repository(#[from] FriendRepositoryError)
+}
+
+#[derive(Clone, Debug)]
+enum DomainEvent {
+    PostCreated {
+        user_id: Uuid,
+        post: Post,
+        followers: Vec<Uuid>,
+    },
+    PostUpdated {
+        user_id: Uuid,
+        post: Post,
+        followers: Vec<Uuid>,
+    },
+    PostDeleted {
+        user_id: Uuid,
+        post_id: Uuid,
+        followers: Vec<Uuid>,
+    },
 }
 
 #[async_trait]
@@ -26,7 +45,7 @@ where
     S: PostService {            
     repository: F,
     service: S,
-    listeners: Vec<Arc<dyn PostListener + Send + Sync>>
+    event_bus: EventBus
 }
 
 impl <F, S> FollowersServiceImpl<F, S>
@@ -37,12 +56,42 @@ where
         FollowersServiceImpl { 
             repository,
             service,
-            listeners
+            event_bus: EventBus::new(100, listeners)
         }
     }
 
     async fn fetch_followers(&self, user_id: Uuid) -> Result<Vec<Uuid>, FollowersServiceError> {
         Ok(self.repository.get_followers_ids(user_id).await?)
+    }
+}
+
+struct EventBus {
+    sender: tokio::sync::mpsc::Sender<DomainEvent>,
+}
+
+impl EventBus {
+    pub fn new(capacity: usize, listeners: Vec<Arc<dyn PostListener + Send + Sync>>) -> Self {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(capacity);
+        tokio::spawn(async move {
+            while let Some(event) = rx.recv().await {                
+                let futures = listeners.iter().map(|l| {
+                    match &event {
+                        DomainEvent::PostCreated { user_id, post, followers } => l.create(&user_id, &followers, &post),                
+                        DomainEvent::PostUpdated { user_id, post, followers } => l.update(&user_id, &followers, &post),
+                        DomainEvent::PostDeleted { user_id, post_id, followers } => l.delete(&user_id, &followers, &post_id)
+                    }
+                });
+                futures::future::join_all(futures).await;                
+            }
+        });
+
+        Self { sender: tx }
+    }
+
+    async fn publish(&self, event: DomainEvent) {
+        if let Err(e) = self.sender.send(event).await {
+            info!("Failed to publish event: {:?}", e);
+        }
     }
 }
 
@@ -55,12 +104,14 @@ where
         let post = self.service.create(user_id, text).await?;   
         match self.fetch_followers(user_id).await {
             Ok(followers) => {
-                for listener in &self.listeners {
-                    listener.create(&user_id, &followers, &post).await;
-                }
+                self.event_bus.publish(DomainEvent::PostCreated {
+                    user_id,
+                    post: post.clone(),
+                    followers,
+                }).await;
             },
             Err(e) => warn!("Failed to fetch followers {:?}", e)
-        };        
+        };                
         Ok(post)
     }
 
@@ -68,9 +119,11 @@ where
         let post = self.service.update(user_id, post_id, text).await?;   
         match self.fetch_followers(user_id).await {
             Ok(followers) => {
-                for listener in &self.listeners {
-                    listener.update(&user_id, &followers, &post).await;
-                }
+                self.event_bus.publish(DomainEvent::PostUpdated {
+                    user_id,
+                    post: post.clone(),
+                    followers,
+                }).await;
             },
             Err(e) => warn!("Failed to fetch followers {:?}", e)
         };        
@@ -81,9 +134,11 @@ where
         let _ = self.service.delete(user_id, post_id).await?;
         match self.fetch_followers(user_id).await {
             Ok(followers) => {
-                for listener in &self.listeners {
-                    listener.delete(&user_id, &followers, &post_id).await;
-                }
+                self.event_bus.publish(DomainEvent::PostDeleted {
+                    user_id,
+                    post_id,
+                    followers,
+                }).await;
             },
             Err(e) => warn!("Failed to fetch followers {:?}", e)
         };        
